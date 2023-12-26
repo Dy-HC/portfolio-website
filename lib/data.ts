991ec2882b48af73bfcae16a679f94e421d0897d
Dy-HC <dupejd1@gmail.com>
2023-12-26 18:25:52 +0700

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -76,12 +76,11 @@ export const projectsData = [
     imageUrl: rmtdevImg,
   },
   {
-    title: "App/Web Kelas",
+    title: "Word Analytics",
     description:
-      "Saya membuat App/Web kelas saya selama berminggu-minggu. Ini dapat memudahkan murid 8A dalam melihat jadwal Piket dan Pelajaran. 
-      Jadwal Piket dan Pelajaran menggunakan algoritma Javascript dari hari Senin - Jumat (Tidak dipakai ataupun dihargai oleh semua teman saya)",
-    tags: ["React", "Vite.js", "Firebase", "Tailwind", "Javascript"],
-    imageUrl: corpcommentImg,
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: wordanalyticsImg,
   },
 ] as const;
 
