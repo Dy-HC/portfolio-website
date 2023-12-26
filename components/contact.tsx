8237e82c770a04880998179a8162114337e08c1b
Dy-HC <dupejd1@gmail.com>
2023-12-26 08:47:13 +0700

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -32,11 +32,8 @@ export default function Contact() {
       <SectionHeading>Contact me</SectionHeading>
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
-        Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
-          example@gmail.com
-        </a>{" "}
-        or through this form.
+        Kirimi saya pesan ;){" "}
+        melalui formulir di bawah ini.
       </p>
 
       <form
@@ -49,7 +46,7 @@ export default function Contact() {
             return;
           }
 
-          toast.success("Email sent successfully!");
+          toast.success("Email berhasil dikirim!");
         }}
       >
         <input
@@ -58,12 +55,12 @@ export default function Contact() {
           type="email"
           required
           maxLength={500}
-          placeholder="Your email"
+          placeholder="Email anda"
         />
         <textarea
           className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="message"
-          placeholder="Your message"
+          placeholder="Pesan anda"
           required
           maxLength={5000}
         />
